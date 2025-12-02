8373c4dff7e4b886350e1f9b0d5a16b32378ba00
JungInGyu <dltnswn20@naver.com>
2025-12-02 23:32:31 +0900

    refactor: enum 상태명 변경 및 entity 예약 날짜 칼럼 추가

--- src/main/java/com/popjub/reservationservice/domain/entity/ReservationStatus.java
+++ src/main/java/com/popjub/reservationservice/domain/entity/ReservationStatus.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ReservationStatus {
 
-	CONFIRMED("예약 확정", "예약이 정상적으로 완료되었습니다"),
+	COMPLETE("예약 확정", "예약이 정상적으로 완료되었습니다"),
 	CANCELLED("예약 취소", "사용자가 예약을 취소했습니다"),
 	NO_SHOW("노쇼", "예약 시간에 나타나지 않았습니다"),
 	CHECKED_IN("체크인 완료", "현장 체크인이 완료되었습니다");
