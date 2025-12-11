ea025f2fa8c5ef4f5a4a74c5e84d2c0f37800301
JungInGyu <dltnswn20@naver.com>
2025-12-11 22:24:21 +0900

    refactor: FeignClient 예약 완료 알림 요청

---
+++ src/main/java/com/popjub/reservationservice/application/port/NotificationPort.java
@@ -0,0 +1,13 @@
+package com.popjub.reservationservice.application.port;
+
+import java.time.LocalTime;
+
+import com.popjub.reservationservice.domain.entity.Reservation;
+
+public interface NotificationPort {
+	void sendReservationCreateNotification(
+		Reservation reservation,
+		String storeName,
+		LocalTime startTime
+	);
+}
---
+++ src/main/java/com/popjub/reservationservice/infrastructure/client/NotificationServiceClient.java
@@ -0,0 +1,16 @@
+package com.popjub.reservationservice.infrastructure.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.popjub.reservationservice.infrastructure.client.dto.ReservationNotificationRequest;
+
+@FeignClient(name = "notification-service", url = "/internal/notifications")
+public interface NotificationServiceClient {
+
+	@PostMapping("/reservation/create")
+	void sendReservationCreateNotification(
+		@RequestBody ReservationNotificationRequest request
+	);
+}
