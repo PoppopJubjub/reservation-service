60f3b67d333d93dbe0d9a98adbbdce46f34af02c
JungInGyu <dltnswn20@naver.com>
2025-12-11 20:27:21 +0900

    remove : 구조 변경에 따른 스토어 잔여석 응답 및 요청 DTO 삭제

--- src/main/java/com/popjub/reservationservice/presentation/dto/request/GetRemainingRequest.java
+++
@@ -1,9 +0,0 @@
-package com.popjub.reservationservice.presentation.dto.request;
-
-import java.util.List;
-import java.util.UUID;
-
-public record GetRemainingRequest(
-	List<UUID> timeslotIds
-) {
-}
